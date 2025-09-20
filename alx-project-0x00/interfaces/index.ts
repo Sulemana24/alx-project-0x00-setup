8de5ccd41660c2191ef35f645cedb1d43c8f9311
Iddrisu_Sulemana <iddrisusulemana665@gmail.com>
2025-09-20 21:35:45 +0000

    Add Next.js project files (fixed submodule issue)

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -6,5 +6,5 @@ export interface ButtonProps {
   title: string;
   size?: "small" | "medium" | "large";
   shape?: "rounded-sm" | "rounded-md" | "rounded-lg" | "rounded-full";
-  styles?: string;
+  styles: string;
 }
